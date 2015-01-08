67f8b15cd130eb56db0e782b51320e8b8ee2f600
Phil Corbett <corbett.phil@gmail.com>
2015-01-08 12:39:11 -0500

    visuals and basic logic

--- core.js
+++ core.js
@@ -1,6 +1,28 @@
-(function () {
-  var sayings = [
-    'How are you coming on that thing I asked about about earlier?',
-    'I\'m going to need all hands on deck for this one.'
-  ];
+var AutoManager = (function () {
+    var that = {},
+        sayings = [
+            'How are you coming on that thing I asked about about earlier?',
+            'I\'m going to need all hands on deck for this one.'
+        ];
+
+    // Assesses the situation and provides guidance.
+    function getNewSaying() {
+        var i = Math.floor(Math.random() * 12000) % sayings.length;
+        
+        return sayings[i];
+    }
+
+    // Executes the communication plan.
+    function writeSaying(saying, author) {
+        document.getElementById('outSaying').innerText = saying;
+        document.getElementById('outAuthor').innerText = author;
+
+    }
+
+    // Bestows wisdom.
+    that.AdviseMe = function () {
+        writeSaying(getNewSaying(), 'AutoManager');
+    };
+
+    return that;
 }());
